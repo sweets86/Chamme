f6fa7ea6e63f7c96049047c87e551d037f3ca540
yanica svensson <sweets86_@hotmail.com>
2021-01-28 13:28:55 +0100

    ...

--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import PaymentStyled from "./PaymentStyled";
 
 interface Props {
-    forms: () => void
+    forms: any
 }
 
 export default function Payment(props: Props) {
